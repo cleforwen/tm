8be746a2d1dacdae85ab1d0fd6eac345a09256a1
cleforwen <cleforwen.fernandez@paranor.ch>
2017-04-21 15:59:49 +0800

    Initial

---
+++ src/main/java/ch/paranor/timelog/TmApplication.java
@@ -0,0 +1,12 @@
+package ch.paranor.timelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmApplication.class, args);
+	}
+}
